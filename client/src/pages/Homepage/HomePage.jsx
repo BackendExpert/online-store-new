868c201cc78f5601e28b718dfe51408df23e26f0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-05-20 07:38:01 +0530

    Update

--- client/src/pages/Homepage/HomePage.jsx
+++ client/src/pages/Homepage/HomePage.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import SecNav from '../../components/Nav/SecNav'
 import TopBanner from '../../components/Banners/TopBanner'
+import TodayItems from './TodayItems'
 
 const HomePage = () => {
-  return (
-    <div>
-        <SecNav />
-        <TopBanner />
-        <h1 className="text-red-500 font-bold">HomePage</h1>
-    </div>
-  )
+    return (
+        <div>
+            <SecNav />
+            <TopBanner />
+            <div className="xl:mx-32 md:mx-12 mx-4">
+                <TodayItems />
+            </div>
+
+        </div>
+    )
 }
 
 export default HomePage
---
+++ client/src/pages/Homepage/TodayItems.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const TodayItems = () => {
+    return (
+        <div className='my-16'>
+            <h1 className="text-orange-500 font-extrabold text-xl text-center uppercase">
+                Today's hot Deals
+            </h1>
+        </div>
+    )
+}
+
+export default TodayItems
