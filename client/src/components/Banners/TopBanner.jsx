3f1718f9a32093e8492dbb3d4af5b6c9ad696eae
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-05-20 07:01:00 +0530

    Create TopBanner.jsx

---
+++ client/src/components/Banners/TopBanner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TopBanner = () => {
+  return (
+    <div>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Beatae aliquid commodi itaque quis explicabo culpa voluptates, atque fugiat molestias non ab, maxime quidem. Nisi, mollitia? Aliquam minus iure laudantium illo.
+    </div>
+  )
+}
+
+export default TopBanner
