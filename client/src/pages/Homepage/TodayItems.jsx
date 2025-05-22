785112f1d0e6dd85a2855f9d5b05f57651dd9704
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-05-22 06:30:05 +0530

    Update

--- client/src/pages/Homepage/TodayItems.jsx
+++ client/src/pages/Homepage/TodayItems.jsx
@@ -8,7 +8,7 @@ const TodayItems = () => {
                 Today's Hot Deals
             </h1>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
                 {todatedeals.map((product, index) => (
                     <div
                         key={index}
--- client/src/pages/Homepage/TopPrdocuts.jsx
+++ client/src/pages/Homepage/TopPrdocuts.jsx
@@ -8,7 +8,7 @@ const TopPrdocuts = () => {
                 Top Deals
             </h1>
 
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 gap-6">
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
                 {todatedeals.map((product, index) => (
                     <div
                         key={index}
